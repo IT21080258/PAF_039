7a4fb596ad06a53c67be15e3cff3740ba8dc0b98
IT21080258 <it21080258@my.sliit.lk>
2023-05-11 11:24:49 +0530

    errors in backend fixed

--- Backend/grp39/src/main/java/com/grp39/project/grp39/repository/ReviewRepository.java
+++ Backend/grp39/src/main/java/com/grp39/project/grp39/repository/ReviewRepository.java
@@ -3,7 +3,6 @@ package com.grp39.project.grp39.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-
 import com.grp39.project.grp39.model.Review;
 
 public interface ReviewRepository extends MongoRepository<Review,String> {
